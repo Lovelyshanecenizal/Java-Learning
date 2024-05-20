d38f0f8ac1c9dec4d9af2182decfbf817e05fca4
Lovely Shane Cenizal <157241603+Lovelyshanecenizal@users.noreply.github.com>
2024-05-20 21:27:58 +0800

    Add files via upload

---
+++ VariablesAndDatatypeTEST.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author 63956
+ */
+public class VariablesAndDatatype {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("test");
+        char bloodType ='O';
+        String name = "David";
+        boolean iStall = false;
+        int age = 24;
+        float gpa = 2.25f;
+//        double grade1 = 93.25;
+        System.out.println("Hi my name is " + name + " Sdpt");
+        System.out.println("I'am " + age + "Years old");
+        System.out.println("My GPA is " + gpa);
+        System.out.println("My blood type is " + bloodType);
+
+        
+           }
+    
+}
+
+        
+//        System.out.println(grade1);        
+//        System.out.println(name);
+//        
+//        name = "Alenere";
+//        iStall = true;
+//        num3 = 10;
+//        
+//        System.out.println(name);
+//            System.out.println(iStall);
+//            
+
+                
+        // TODO code application logic here
+ 
